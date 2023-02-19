a62aa7d9ab5d555dfae28645515361efcbd71c7f
Mateus A. Kreuch <mateus.kreuch@gmail.com>
2023-02-18 23:24:09 -0300

    Initial commit

---
+++ src/main/java/com/ukrech/Tabletop.java
@@ -0,0 +1,15 @@
+package com.ukrech;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tabletop implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("tabletop");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
